a48bd60e6edbe7dbb689438d5dd126f8d2022110
sarascavone <163124505+sarascavone@users.noreply.github.com>
2026-02-06 18:24:46 +0100

    Update Noleggio.java

--- src/Noleggio.java
+++ src/Noleggio.java
@@ -2,42 +2,46 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Noleggio {
-
     private static int counter = 1;
 
     private int id;
+    private LocalDateTime dataInizio;
+    private LocalDateTime dataFine;
+    private int kmIniziali;
+    private double costoTotale; 
+    
     private Cliente cliente;
     private Mezzo mezzo;
     private ITariffa tariffa;
+    private PuntoNoleggio puntoNoleggio;
+    
+    private StatoNoleggio statoNoleggio;
+    private StatoPagamento statoPagamento;
 
-    private LocalDateTime dataInizio;
-    private LocalDateTime dataFine;
-
-    public Noleggio(Cliente c, Mezzo m, ITariffa t) {
+    public Noleggio(Cliente cliente, Mezzo mezzo, ITariffa tariffa, PuntoNoleggio punto) {
         this.id = counter++;
-        this.cliente = c;
-        this.mezzo = m;
-        this.tariffa = t;
+        this.cliente = cliente;
+        this.mezzo = mezzo;
+        this.tariffa = tariffa;
+        this.puntoNoleggio = punto;
         this.dataInizio = LocalDateTime.now();
+        this.statoNoleggio = StatoNoleggio.ATTIVO;
     }
 
-    public void chiudi() {
-        this.dataFine = LocalDateTime.now();
+    public void chiudi(LocalDateTime dataFine, PuntoNoleggio puntoConsegna) {
+        this.dataFine = dataFine;
+        this.puntoNoleggio = puntoConsegna;
+        this.statoNoleggio = StatoNoleggio.CONCLUSO;
     }
 
-    public double calcolaCostoFinale() {
-        long minuti = Duration.between(
-                dataInizio, dataFine
-        ).toMinutes();
-
-        return tariffa.calcolaCosto((int) minuti, 0);
+    public double calcolaCostoFinale(int kmFinali) {
+        long minuti = Duration.between(dataInizio, dataFine).toMinutes();
+        int kmPercorsi = kmFinali - this.kmIniziali;
+        
+        this.costoTotale = tariffa.calcolaCosto((int) minuti, kmPercorsi);
+        return this.costoTotale;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public Mezzo getMezzo() {
-        return mezzo;
-    }
+    public int getId() { return id; }
+    public Mezzo getMezzo() { return mezzo; }
 }
